22ec49bfd2b2c8c41b7abb1b95b8d2295b2118e8
Yujung Gil <74306759+fairyroad@users.noreply.github.com>
2023-01-06 11:22:01 +0900

    Update 1_TwoSum_Array.py

--- fairyroad/LeetCode/1_TwoSum_Array.py
+++ fairyroad/LeetCode/1_TwoSum_Array.py
@@ -1,11 +1,7 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        for i in range(len(nums) - 1):
-            for j in range(i+1, len(nums)):
+        len_num = len(nums)
+        for i in range(len_num - 1):
+            for j in range(i+1, len_num):
                 if nums[i] + nums[j] == target:
                     return [i, j]
