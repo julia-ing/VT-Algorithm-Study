a79eb9703935b89d31254eb9579f5163f9298a16
eileen jang <eileen.jang2139@gmail.com>
2023-01-19 22:28:51 +0900

    Invert Binary Tree - adds readable version

--- eileenjang/LeetCode/226_Invert_Binary_Tree.java
+++ eileenjang/LeetCode/226_Invert_Binary_Tree.java
@@ -16,10 +16,9 @@
 class Solution {
     public TreeNode invertTree(TreeNode root) {
         if (root == null) { return null; }
-        TreeNode right = invertTree(root.right);
-        TreeNode left = invertTree(root.left);
-        root.left = right;
-        root.right = left;
+        TreeNode temp = invertTree(root.left);
+        root.left = invertTree(root.right);
+        root.right = temp;
         return root;
     }
 }
