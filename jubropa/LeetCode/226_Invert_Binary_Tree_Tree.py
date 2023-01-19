827c7ca1d56923a702fa8f2be763b9c81eec1599
jubropa <99578312+jubropa@users.noreply.github.com>
2023-01-19 16:09:58 +0900

    [박준형] week3

--- jubropa/LeetCode/226_Invert_Binary_Tree_Tree.py
+++ jubropa/LeetCode/226_Invert_Binary_Tree_Tree.py
@@ -1,43 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        
-        self.m = len(heights)
-        self.n = len(heights[0])
-        self.heights = heights
-        
-        pacific_coords = set()
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        invertedTree = self.recursionInvert(root)
+        return invertedTree
 
-        for i in range(self.m):
-            self.DFS(i, 0, 0, pacific_coords)
-        for j in range(self.n):
-            self.DFS(0, j, 0, pacific_coords)
-
-        atlantic_coords = set()
-
-        for i in range(self.m):
-            self.DFS(i, self.n-1, 0, atlantic_coords)
-        for j in range(self.n):
-            self.DFS(self.m-1, j, 0, atlantic_coords)
-
-        return sorted(list(pacific_coords & atlantic_coords))
+    def recursionInvert(self, node:Optional[TreeNode]) -> None:
+        if node!=None:
+            tmp_left = node.left
+            node.left = self.recursionInvert(node.right)
+            node.right = self.recursionInvert(tmp_left)
+        return node
 
 
-    def DFS(self, i:int, j:int, prev_height:int, coords:set[tuple[int, int]]) -> None:
-
-        if i==-1 or i==self.m or j==-1 or j==self.n:
-            return
-
-        if (i, j) in coords:
-            return
-
-        cur_height = self.heights[i][j]
-
-        if cur_height < prev_height:
-            return
-
-        coords.add((i, j))
-
-        self.DFS(i-1, j, cur_height, coords)
-        self.DFS(i+1, j, cur_height, coords)
-        self.DFS(i, j-1, cur_height, coords)
-        self.DFS(i, j+1, cur_height, coords)
