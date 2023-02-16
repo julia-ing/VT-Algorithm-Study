1bfb47680174139a0058d999d45f9faadc654181
dlanals <97150219+dlanals@users.noreply.github.com>
2023-02-16 20:17:47 +0900

    Create 230_Kth Smallest Element in a BST_Tree.py

---
+++ dlanals/LeetCode/230_Kth Smallest Element in a BST_Tree.py
@@ -0,0 +1,12 @@
+# iterative
+def kthSmallest(root, k):
+    stack = []
+    while root or stack:
+        while root:
+            stack.append(root)
+            root = root.left
+        root = stack.pop()
+        k -= 1
+        if k == 0:
+            return root.val
+        root = root.right
