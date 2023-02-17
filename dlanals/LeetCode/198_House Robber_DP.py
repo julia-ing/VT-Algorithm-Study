bc66b9291ab85184a51f21946fc0def2e563f6ce
dlanals <97150219+dlanals@users.noreply.github.com>
2023-02-18 02:21:40 +0900

    Update 198_House Robber_DP.py
    코드 정리

--- dlanals/LeetCode/198_House Robber_DP.py
+++ dlanals/LeetCode/198_House Robber_DP.py
@@ -1,16 +1,14 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         n = len(nums)
+        dp = [0] * n
 
-        if n == 0: 
-            return 0
-        
-        robNextPlusOne = 0
-        robNext = nums[n - 1]
+        for i in range(n):
+            if i==0 :
+                dp[0] = nums[0]
+            elif i==1 :
+                dp[1] = max(dp[0], nums[1])
+            else:
+                dp[i] = max(dp[i-1], dp[i-2] + nums[i])
 
-        for i in range(n - 2, -1, -1):
-            current = max(robNext, robNextPlusOne + nums[i])
-            robNextPlusOne = robNext
-            robNext = current
-        
-        return robNext
+        return dp[n-1]
