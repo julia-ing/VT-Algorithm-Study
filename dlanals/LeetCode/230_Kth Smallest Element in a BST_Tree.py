dbc1962eec3b531dde588e4a26400ab9eb827d7e
dlanals <97150219+dlanals@users.noreply.github.com>
2023-02-17 10:34:48 +0900

    Update 230_Kth Smallest Element in a BST_Tree.py
    solution 추가

--- dlanals/LeetCode/230_Kth Smallest Element in a BST_Tree.py
+++ dlanals/LeetCode/230_Kth Smallest Element in a BST_Tree.py
@@ -1,12 +1,35 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# Recursive
+# O(n)
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+
+        def tree_to_list(root) -> list:
+            if not root: # 노드가 더이상 존재하지 않을 때
+                return []
+
+            return tree_to_list(root.left) + [root.val] + tree_to_list(root.right)
+        
+        tree_list = tree_to_list(root)
+        return tree_list[k-1]
+
+
 # iterative
-def kthSmallest(root, k):
-    stack = []
-    while root or stack:
-        while root:
-            stack.append(root)
-            root = root.left
-        root = stack.pop()
-        k -= 1
-        if k == 0:
-            return root.val
-        root = root.right
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        stack = []
+        while root or stack:
+            while root:
+                stack.append(root)
+                root = root.left
+            root = stack.pop()
+            k -= 1
+            if k == 0:
+                return root.val
+            root = root.right
