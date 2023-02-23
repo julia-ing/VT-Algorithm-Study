ac8c578e5ba84da6959dcf4894715161ddc7f363
dlanals <97150219+dlanals@users.noreply.github.com>
2023-02-24 03:06:00 +0900

    Update 128_LongestConsecutiveSequence_Graph.py
    코드 정리

--- dlanals/LeetCode/128_LongestConsecutiveSequence_Graph.py
+++ dlanals/LeetCode/128_LongestConsecutiveSequence_Graph.py
@@ -1,18 +1,19 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        nums = sorted(set(nums))
-        
-        cur_max = 0
-        cur_count = 0
-        prev = None
-        for i in nums:
-            if prev is not None:
-                if prev+1 == i:
-                    cur_count += 1
-                else:
-                    cur_max = max(cur_max, cur_count)
-                    cur_count = 1
+        nums.sort()
+
+        cnt = 1
+        max_length = 1
+
+        if len(nums) == 0: return 0
+
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i-1] + 1:
+                cnt += 1
+            elif nums[i] == nums[i-1]:
+                continue
             else:
-                cur_count += 1
-            prev = i
-        return max(cur_max, cur_count)
+                if cnt > max_length : max_length = cnt
+                cnt = 1
+            
+        return max(cnt, max_length)
