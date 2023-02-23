af77ad39da4a804ca477793027a97e1b1b77eec0
dlanals <97150219+dlanals@users.noreply.github.com>
2023-02-24 03:06:38 +0900

    Update 295_FindMedianFromDataStream_Heap.py
    코드 정리

--- dlanals/LeetCode/295_FindMedianFromDataStream_Heap.py
+++ dlanals/LeetCode/295_FindMedianFromDataStream_Heap.py
@@ -1,15 +1,22 @@
-from sortedcontainers import SortedList
 class MedianFinder:
 
     def __init__(self):
-        self.arr=SortedList([])
+      self.arr = [] 
 
     def addNum(self, num: int) -> None:
-        self.arr.add(num)
-        
+        self.arr.append(num)
+
     def findMedian(self) -> float:
-        n=len(self.arr)
-        if(n%2==0):
-            return(self.arr[n//2]+self.arr[n//2-1])/2
+        self.arr.sort()
+        l = len(self.arr)
+        
+        if l%2 == 1:
+            return self.arr[l//2]
         else:
-            return(self.arr[n//2])
+            return (self.arr[l//2-1] + self.arr[l//2])/2
+
+        
+# Your MedianFinder object will be instantiated and called as such:
+# obj = MedianFinder()
+# obj.addNum(num)
+# param_2 = obj.findMedian()
