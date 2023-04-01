73a87af6b149b3a126fe5184c838d1db49e37fc2
Jiho Yoo <goll167@gmail.com>
2023-04-02 00:54:00 +0900

    Create 371_Sum of Two Integers_Binary.py

---
+++ Ryujiho/LeetCode/371_Sum of Two Integers_Binary.py
@@ -0,0 +1,16 @@
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        mask = 0xffffffff
+
+        while (b & mask) > 0:
+            carry = (a & b) << 1
+            a = a ^ b
+            b = carry
+        return (a & mask) if b > 0 else a # Consider negative number
+      
+      
+'''
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        return eval(f'{a}{chr(43)}{b}')
+'''
